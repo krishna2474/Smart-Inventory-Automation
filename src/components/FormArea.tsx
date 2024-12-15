c6a57862c95756e908cc9d20f32160dac8554395
Krishna <krishnakukreja85@gmail.com>
2024-12-15 14:39:00 +0530

    Adjusted Top Margin of the FormArea on smaller screen

--- src/components/FormArea.tsx
+++ src/components/FormArea.tsx
@@ -23,7 +23,7 @@ export const FormArea = ({ type }: FormAreaProps) => {
         transition: { duration: 1, type: "spring", stiffness: 30 }, // Smooth spring animation
       }}
       className={`relative  flex justify-center items-center top-0 min-h-screen font-nunito ${
-        isMenuOpen ? "mt-64 sm:mt-0" : "sm:mt-0 mt-[-15px]"
+        isMenuOpen ? "mt-64 sm:mt-0" : "mt-[-25px] sm:mt-0"
       }`}
     >
       <div
