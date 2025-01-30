3d3acdca8b575bd14a66c77b2a5477fdb02569f1
Krishna <krishnakukreja85@gmail.com>
2025-01-30 16:36:03 +0530

    Login space

--- src/components/FormArea.tsx
+++ src/components/FormArea.tsx
@@ -13,54 +13,55 @@ type FormAreaProps = {
 export const FormArea = ({ type }: FormAreaProps) => {
   const isMenuOpen = useRecoilValue(menuState);
   return (
-    <motion.div
-      initial={{
-        opacity: 0,
-        y: 100,
-      }}
-      animate={{
-        opacity: 1,
-        y: 0,
-        transition: { duration: 1, type: "spring", stiffness: 30 }, // Smooth spring animation
-      }}
-      className={`relative  flex justify-center top-0 min-h-screen font-nunito ${
-        isMenuOpen ? "mt-64 sm:mt-0" : "mt-[-25px] sm:mt-0"
-      }`}
-    >
-      <div
-        className={`mr-10 ml-10 rounded-3xl h-auto ${
-          type === "signin" ? "sm:h-[480px]" : "sm:h-full"
-        } w-full sm:w-[850px] bg-white/10 backdrop-blur-3xl border border-white/50 `}
+    <div className="w-full min-h-screen h-full bg-cover bg-center">
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 100,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 1, type: "spring", stiffness: 30 }, // Smooth spring animation
+        }}
+        className={`relative flex justify-center top-0 min-h-screen sm:min-h-[calc(100vh-64px)] font-nunito ${
+          isMenuOpen ? "mt-64 sm:mt-0" : "mt-[-25px] sm:mt-0"
+        }`}
       >
-        <div className="flex justify-center pt-10 font-bebas text-5xl sm:text-6xl text-white">
-          {type === "signup" ? "Signup" : "Login"}
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 p-5 sm:p-10">
-          <div className="flex flex-col items-center">
-            <div className="hidden sm:block">
-              <img
-                className="relative bottom-8"
-                src={"/assets/Smart Inventory Automation-Dark.png"}
-                width={320}
-                height={110}
-                alt="Logo"
-              />
+        <div
+          className={`mr-10 ml-10 rounded-3xl h-auto ${
+            type === "signin" ? "sm:h-[480px]" : "sm:h-full"
+          } w-full sm:w-[850px] bg-white/10 backdrop-blur-3xl border border-white/50`}
+        >
+          <div className="flex justify-center pt-10 font-bebas text-5xl sm:text-6xl text-white">
+            {type === "signup" ? "Signup" : "Login"}
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-5 sm:p-8">
+            <div className="flex flex-col items-center">
+              <div className="hidden sm:block">
+                <img
+                  className="relative bottom-8"
+                  src={"/assets/Smart Inventory Automation-Dark.png"}
+                  width={320}
+                  height={110}
+                  alt="Logo"
+                />
+              </div>
+              <div className="space-y-4 flex flex-col">
+                <ContinueWithGoogle />
+                <ContinueWithFacebook />
+              </div>
             </div>
-            <div className="space-y-5 flex flex-col">
-              <ContinueWithGoogle />
-
-              <ContinueWithFacebook />
+            <div className="flex justify-center">
+              {type === "signup" ? (
+                <SignupForm type={type} />
+              ) : (
+                <SigninForm type={type} />
+              )}
             </div>
           </div>
-          <div className="flex justify-center">
-            {type === "signup" ? (
-              <SignupForm type={type} />
-            ) : (
-              <SigninForm type={type} />
-            )}
-          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 };
